7a8b685625e21750bcf8d7126989fdfb869184b6
prathmesh-ghatmal <mrjpg2311@gmail.com>
2024-04-04 07:17:30 +0530

    home screen done

---
+++ app/src/main/java/com/example/admincravecart/AddItemsActivity.java
@@ -0,0 +1,14 @@
+package com.example.admincravecart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_items);
+    }
+}
