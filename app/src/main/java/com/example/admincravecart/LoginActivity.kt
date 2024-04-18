e72b778136cb3251ff6ce996af7d4cc8aaa36cc6
prathmesh-ghatmal <mrjpg2311@gmail.com>
2024-04-18 17:44:43 +0530

    authentication done

--- app/src/main/java/com/example/admincravecart/LoginActivity.kt
+++ app/src/main/java/com/example/admincravecart/LoginActivity.kt
@@ -1,39 +1,41 @@
 package com.example.admincravecart
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.admincravecart.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.FirebaseException
-import com.google.firebase.R
+
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
+
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.database
 import com.google.firebase.ktx.Firebase
-import kotlin.math.log
+
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var email: String
     private lateinit var password: String
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-   private lateinit var googlesignInclient:GoogleSignInClient
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     private val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val googleSignInOptions=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.))
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
         //initialization of firebase Auth
 
         auth = Firebase.auth
@@ -43,7 +45,8 @@ class LoginActivity : AppCompatActivity() {
         database = com.google.firebase.Firebase.database.reference
 
         //initialization of Google signin
-        googlesignInclient=GoogleSignIn.getClient(this,googleSignInOptions)
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
         binding.loginButton.setOnClickListener {
             // getting text from edittexts
             email = binding.NewUserEmailAddress.text.toString().trim()
@@ -54,6 +57,10 @@ class LoginActivity : AppCompatActivity() {
                 loginuser(email, password)
             }
 
+        }
+        binding.GoogleButton.setOnClickListener {
+            val signIntent = googleSignInClient.signInIntent
+            launcher.launch(signIntent)
         }
         binding.Donthavebutton.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
@@ -79,20 +86,23 @@ class LoginActivity : AppCompatActivity() {
                         "invalid email or password",
                         Toast.LENGTH_SHORT,
                     ).show()
-                }else{
+                } else {
                     if (message!!.contains("We have blocked all requests from this device due to unusual activity")) {
                         Toast.makeText(
                             baseContext,
                             "Blocked due to too many failed attempts",
                             Toast.LENGTH_SHORT,
                         ).show()
-                    }else{
+                    } else {
                         if (message!!.contains("The email address is badly formatted")) {
                             Toast.makeText(
                                 baseContext,
                                 "Enter valid email",
                                 Toast.LENGTH_SHORT,
-                            ).show()}else{ Toast.makeText(this,"login failed",Toast.LENGTH_SHORT).show()}
+                            ).show()
+                        } else {
+                            Toast.makeText(this, "login failed", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
@@ -103,4 +113,28 @@ class LoginActivity : AppCompatActivity() {
     private fun UpdateUi(user: FirebaseUser?) {
         startActivity(Intent(this, MainActivity::class.java))
     }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (task.isSuccessful) {
+                    val account: GoogleSignInAccount = task.result
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    auth.signInWithCredential(credential).addOnCompleteListener { authTask ->
+                        if (authTask.isSuccessful) {
+                            Toast.makeText(this, "Sucessfully logged in", Toast.LENGTH_LONG).show()
+                            UpdateUi(user = null)
+                            finish()
+                        } else {
+                            Toast.makeText(this, "log in failed", Toast.LENGTH_LONG).show()
+
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(this, "log in failed", Toast.LENGTH_LONG).show()
+
+            }
+        }
 }
